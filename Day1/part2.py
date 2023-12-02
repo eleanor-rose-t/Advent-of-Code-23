20f8fad371bf8cef89d8c3f8af2b3e3d912ad476
GeNiUSgirl14 <56850565+GeNiUSgirl14@users.noreply.github.com>
2023-12-02 02:30:44 -0500

    find_line_value() working as expected - find_sum() not working as expected

--- Day1/part2.py
+++ Day1/part2.py
@@ -1,37 +1,42 @@
+import re
+
 def find_line_value(line):
     """
     Sums the first digit and last digit in the line accounting for spelled out digits.
     """
     spelled_out_digits = {
-        "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
+        "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+        "six": "6", "seven": "7", "eight": "8", "nine": "9"
     }
-    first_digit = next((char for char in line.split() if char.isdigit() or char in spelled_out_digits), None)
-    last_digit = next((char for char in reversed(line.split()) if char.isdigit() or char in spelled_out_digits), None)
+    pattern = r'(?:\d|one|two|three|four|five|six|seven|eight|nine)'
+
+    matches = re.findall(pattern, line, re.IGNORECASE)
+
+    if matches:
+        first_digit = matches[0]
+        if not first_digit.isdigit():
+            first_digit = spelled_out_digits.get(first_digit)
+        last_digit = matches[-1]
+        if not last_digit.isdigit():
+            last_digit = spelled_out_digits.get(last_digit)
     
-    if first_digit in spelled_out_digits:
-        first_digit = spelled_out_digits[first_digit]
-        
-    if last_digit in spelled_out_digits:
-        last_digit = spelled_out_digits[last_digit]
-    
-    if first_digit is not None and last_digit is not None:
-        line_value = int(first_digit + last_digit)
-        return line_value
-    else:
-        return 0
-    
+    return int(first_digit + last_digit)
+
+
 def find_sum(filename):
     """
     Sums the line values of each line in the file.
     """
+    sum = 0
     with open(filename) as file:
         lines = file.readlines()
-        line_values = [find_line_value(line.strip()) for line in lines]
-        total_sum = sum(line_values)
-        return total_sum
+        for line in lines:
+            line_value = find_line_value(line)
+            sum += line_value
+        return sum
     
 def main():
-    total_sum = find_sum("input.txt")
+    total_sum = find_sum("/Users/eleanorrose/Programming/Advent-of-Code-23/Advent-of-Code-23/Day1/input.txt")
     print(total_sum)
     
 if __name__ == "__main__":
