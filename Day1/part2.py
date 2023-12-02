c923eaac71dd9f3a7ddea8d6fda517aeeab15e53
GeNiUSgirl14 <56850565+GeNiUSgirl14@users.noreply.github.com>
2023-12-02 01:41:05 -0500

    Attempted to refactor find_line_value() to get correct answer - not performing as expected

--- Day1/part2.py
+++ Day1/part2.py
@@ -5,8 +5,14 @@ def find_line_value(line):
     spelled_out_digits = {
         "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
     }
-    first_digit = next((char for char in line if char.isdigit() or char in spelled_out_digits), None)
-    last_digit = next((char for char in reversed(line) if char.isdigit() or char in spelled_out_digits), None)
+    first_digit = next((char for char in line.split() if char.isdigit() or char in spelled_out_digits), None)
+    last_digit = next((char for char in reversed(line.split()) if char.isdigit() or char in spelled_out_digits), None)
+    
+    if first_digit in spelled_out_digits:
+        first_digit = spelled_out_digits[first_digit]
+        
+    if last_digit in spelled_out_digits:
+        last_digit = spelled_out_digits[last_digit]
     
     if first_digit is not None and last_digit is not None:
         line_value = int(first_digit + last_digit)
@@ -23,3 +29,10 @@ def find_sum(filename):
         line_values = [find_line_value(line.strip()) for line in lines]
         total_sum = sum(line_values)
         return total_sum
+    
+def main():
+    total_sum = find_sum("input.txt")
+    print(total_sum)
+    
+if __name__ == "__main__":
+    main()
