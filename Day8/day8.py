8d6a77fc6810e8b6a8e8fc1a0bec4e61dc47a78f
GeNiUSgirl14 <56850565+GeNiUSgirl14@users.noreply.github.com>
2023-12-08 22:38:48 -0500

    Finished part 1

---
+++ Day8/day8.py
@@ -0,0 +1,44 @@
+def read_input(file_name):
+    input_lines = [x for x in open(file_name).read().strip().split('\n\n')]
+    return input_lines
+
+def parse_connections(input_lines):
+    instructions = list(input_lines[0])
+    connections = {}
+    for line in input_lines[1].split("\n"):
+        start_point = line.split(" ")[0]
+        first_endpoint = line.split("(")[1].split(",")[0]
+        second_endpoint = line.split(" ")[3].split(")")[0]
+        connections[start_point] = (first_endpoint, second_endpoint)
+    return instructions, connections
+
+def calculate_steps(instructions, connections):
+    current_position = 'AAA'
+    index = 0
+    while current_position != 'ZZZ':
+        direction = instructions[index % len(instructions)]
+        current_position = connections[current_position][0 if direction == 'L' else 1]
+        index += 1
+    return index
+
+def solve_steps(start, instructions, connections):
+    current_pos = start
+    index = 0
+    while not current_pos.endswith('Z'):
+        direction = instructions[index % len(instructions)]
+        current_pos = connections[current_pos][0 if direction == 'L' else 1]
+        index += 1
+    return index
+
+def main(): 
+    file_name = "input.txt"
+    input_lines = read_input(file_name)
+    inst, conn = parse_connections(input_lines)
+
+    result = calculate_steps(inst, conn)
+    print(result)
+    
+if __name__ == "__main__":
+    main()
+
+
